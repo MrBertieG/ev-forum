53d156d1b278dd75b43243f701b8993ba00c1895
Bertie <albertogheorghita90@gmail.com>
2022-03-01 19:58:46 +0000

    change allowed hosts to p4-ev-forum.herokuapp.com

--- evforum/settings.py
+++ evforum/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['evforum.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = ['p4-ev-forum.herokuapp.com', 'localhost']
 
 
 # Application definition
