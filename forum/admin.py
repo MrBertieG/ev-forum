84035d2fb7a37a6621e07d2e3cfec37f204bf008
Bertie <albertogheorghita90@gmail.com>
2022-03-01 21:16:30 +0000

    add comment and contact view to the admin page

--- forum/admin.py
+++ forum/admin.py
@@ -10,6 +10,7 @@ class CategoryAdmin(admin.ModelAdmin):
     list_display = ('name', 'desc_box')
     search_fields = ('name', 'desc_box')
 
+
 @admin.register(Post)
 class PostAdmin(SummernoteModelAdmin):
     """Post view in Admin Page"""
@@ -18,3 +19,18 @@ class PostAdmin(SummernoteModelAdmin):
     list_filter = ('author__username', 'created')
     prepopulated_fields = {'slug': ('title',)}
     summernote_fields = ('body',)
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    """Comment view in Admin Page"""
+    list_display = ('post', 'created', 'author')
+    search_fields = ('body', 'author__username')
+    list_filter = ('author__username', 'created')
+
+@admin.register(Contact)
+class ContactAdmin(admin.ModelAdmin):
+    """Contact view in Admin Page"""
+    list_display = ('first_name', 'last_name', 'email', 'created')
+    search_fields = ('first_name', 'last_name', 'email', 'body')
+    list_filter = ('first_name', 'last_name', 'email')
